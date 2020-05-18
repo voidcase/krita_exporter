5c5f0f3666651573a57e93a65577b94e282cff41
Isak Lindhé <isak.e.lindhe@gmail.com>
2020-05-18 22:36:41 +0200

    error handling

--- krita_exporter.py
+++ krita_exporter.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from pathlib import Path
-from sys import stderr, exit
+import sys
 
 def get_args():
     parser = ArgumentParser('krita-exporter')
@@ -8,14 +8,29 @@ def get_args():
     parser.add_argument('dst')
     return parser.parse_args()
 
+def err(text):
+    print(f'ERROR: {text}', file=sys.stderr)
+    sys.exit(1)
+
+def exec_export(src, dst, dry=True):
+    # TODO run command to export
+    pass
+
 def run():
     args = get_args()
     src = Path(args.src)
+    dst = Path(args.dst)
     if not src.is_dir():
-        print(f'no directory at "{src}"', file=stderr)
-        exit(1)
-    print(list(src.glob('**/*.kra')))
-
+        err(f'no directory at "{src}"')
+    if dst.exists() and not dst.is_dir():
+        err(f'"{dst}" exists but is not a directory.')
+    if not dst.is_dir():
+        print(f'no directory at "{dst}", creating...')
+        dst.mkdir()
+    pathgen = src.glob('**/*.kra')
+    for path in pathgen:
+        print(path)
+        pass
 
 if __name__ == '__main__':
     run()
