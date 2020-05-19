3f831e42193cf924c4fddf799047a4a6745930e1
Isak Lindhé <isak.e.lindhe@gmail.com>
2020-05-19 22:20:55 +0200

    Added helptext and shortened lines.

--- krita_exporter.py
+++ krita_exporter.py
@@ -1,16 +1,40 @@
 from argparse import ArgumentParser
 from pathlib import Path
 import sys
-import shutils
+import shutil
 
 def get_args():
     parser = ArgumentParser('krita-exporter')
-    # TODO write help text
-    parser.add_argument('src')
-    parser.add_argument('dst')
-    parser.add_argument('--format', default='png')
-    parser.add_argument('--force', default=False, action='store_true')
-    parser.add_argument('--alt')
+    parser.add_argument(
+        'src',
+        type=str,
+        help='The source directory containing the .kra files you want to export.\
+              This directory will be searched recursively'
+    )
+    parser.add_argument(
+        'dst',
+        type=str,
+        help='The destination directory where you want the exported files to appear. \
+              This directory will have the same directory structure as src \
+              and will be created if it does not exist.'
+    )
+    parser.add_argument(
+        '--format',
+        default='png',
+        type=str,
+        help='The format to export to. Default value: png'
+    )
+    parser.add_argument(
+        '--force',
+        default=False,
+        action='store_true',
+        help='Export even those files which already have an exported file in dst.'
+    )
+    parser.add_argument(
+        '--alt',
+        type=str,
+        help='If krita is called something else in your system. \
+              You can specify it with this argument.')
     return parser.parse_args()
 
 def err(text):
@@ -22,14 +46,18 @@ def export_command(src, dst, dry=True, alt_krita=None):
     return f'{krita} --export {src} --export-filename {dst};'
 
 def krita_is_installed() -> bool:
-    return shutils.which('krita') is not None
+    return shutil.which('krita') is not None
 
 def run():
     args = get_args()
     src = Path(args.src)
     dst = Path(args.dst)
     if not krita_is_installed():
-        err('krita is not found in PATH. I guess you wouldn\'t be trying to run this if you don\'t have Krita installed, so something is weird. This should work when the command "which krita" runs successfully. If krita is called something else on your system, put that in the --alt option.')
+        err('krita is not found in PATH. \
+             I guess you wouldn\'t be trying to run this if you don\'t have Krita installed, \
+             so something is weird. \
+             This should work when the command "which krita" runs successfully. \
+             If krita is called something else on your system, put that in the --alt option.')
     if not src.is_dir():
         err(f'no directory at "{src}"')
     if dst.exists() and not dst.is_dir():
@@ -42,7 +70,7 @@ def run():
         relpath = src_path.relative_to(src)
         dst_relpath = relpath.with_suffix(f'.{args.format}')
         dst_path = dst / dst_relpath
-        #TODO create needed directories
+        dst_path.parent.mkdir(parents=True, exist_ok=True)
         if args.force or not dst_path.exists():
             print(export_command(src_path, dst_path))
 
